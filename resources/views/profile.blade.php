40f0b05d0fbc29acc19e99a84500f2f9a7daa38d
Ivan <106988585+qwike@users.noreply.github.com>
2023-12-16 15:23:56 +0400

    blank

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,2 +1,24 @@
-123
-<a href="{{ route('logout') }}">выйтти</a>
+@extends('layout.app')
+
+@section('title', 'Кабинет')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('/css/profile.css') }}">
+@endsection
+
+@section('sidebar')
+    @include('partials.sidebar')
+@endsection
+
+@section('content')
+    <div class="wrapper">
+        <div class="container">
+            <div class="content_space">
+
+            </div>
+        </div>
+    </div>
+    <script>
+        $('#profile').addClass('sidebar_menu_button_active');
+    </script>
+@endsection
