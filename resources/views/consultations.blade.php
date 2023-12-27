1dd0c5ec844240a11a478865e7f4a12de742839d
maximkasvetofor <maximplay2018@gmail.com>
2023-12-27 19:52:26 +0400

    ХИРУРГИЧЕСКАЯ ЗАМЕНА ЦВЕТА

--- resources/views/consultations.blade.php
+++ resources/views/consultations.blade.php
@@ -93,7 +93,7 @@
                 font-size: 24px;
                 font-family: Roboto;
                 text-align: center;
-                background: #626DFF;
+                background: #858AD4;
                 border: none;
                 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
                 display: flex;
@@ -241,7 +241,7 @@
                 overflow: hidden;
             }
             .consultations_menu{
-                background: #626DFF;
+                background: #858AD4;
                 max-width: 400px;
                 width: 100%;
                 height: 100%;
